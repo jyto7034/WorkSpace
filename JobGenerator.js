3bd6bdb862b816f5fd13bf3b5066aeef9c356ae8
jyto7034 <jyto7034@naver.com>
2021-04-11 19:08:16 +0900

    init

--- JobGenerator.js
+++ JobGenerator.js
@@ -1,3 +1,11 @@
+// function getElementByXpath(path) {
+//     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+//   }
+  
+//   getElementByXpath(##).click()
+//   getElementByXpath(##).value = ""
+
+
 var Workers = {
     "홍승원" : {},
     "김진호" : {},
@@ -8,7 +16,78 @@ var Workers = {
     "전성민" : {},
     "이경현" : {},
     "권예찬" : {},
-    "공재영" : {},
-    "서동원" : {}
+    "공재영": {},
+    "서동원": {}
 }
 
+var time = {
+    "weekday": {
+        "2": {},
+        "3": {},
+        "4": {},
+        "5": {},
+        "6": {},
+        "7": {},
+        "8": {},
+        "9": {}
+    },
+    "holiday" : {
+        "2": {},
+        "3": {},
+        "4": {},
+        "5": {},
+        "6": {},
+        "7": {},
+        "8": {},
+        "9": {}
+    },
+    "night" : {
+        "2": {},
+        "3": {},
+        "4": {},
+        "5": {},
+        "6": {},
+        "7": {},
+        "8": {},
+        "9": {}
+    }
+}
+
+function filter(workers){
+    for(let name in workers){
+        return (Object.keys(workers[name]).length == 0) ? workers[name] : undefined;
+    }
+}
+
+var enableWorkers = filter(Workers);
+var enableWorkersLength = enableWorkers.length;
+
+function Rank(){};
+Rank.prototype.rank = {
+    "day" : {
+        "AM" : {"w":"2"},
+        "PM" : {},
+        "PT" : {}
+    },
+    "night" : {}
+}
+Rank.prototype.getAM = function(){ return Rank.prototype.rank["day"]["AM"]; }
+Rank.prototype.getPM = function(){ return Rank.prototype.rank["day"]["PM"]; }
+Rank.prototype.getPT = function(){ return Rank.prototype.rank["day"]["PT"]; }
+Rank.prototype.getNight = function(){ return Rank.prototype.rank["night"]; }
+
+function LastRank(){}
+LastRank.prototype = Object.create(Rank.prototype);
+LastRank.prototype.constructor = LastRank;
+
+function NewRank(){}
+NewRank.prototype = Object.create(Rank.prototype);
+NewRank.prototype.constructor = NewRank;
+
+var lRank = new LastRank();
+var nRank = new NewRank();
+
+var CurrentFirstWorker = (function(){
+    if(lRank.getAM[0] in enableWorkers)
+    return lRank.getAM[0]
+})
